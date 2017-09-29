893877ac91109f2ad78e4d4ed4fbcadc7ee5c406
Nikita Deshmukh <ndeshmukh@galaxyps89.com>
2017-09-29 09:16:34 -0400

    iisue fixed

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,58 +1,32 @@
 import React, { Component } from 'react'
-import ReactDom  from 'react-dom'
+import ReactDom from 'react-dom'
 
 
 import { connect } from 'react-redux'//glue between react and redux
 
 class BookList extends Component {
-    constructor(props)
-    {     
-        super(props);
-        this.renderLists= this.renderLists.bind(this)
-    }
-    componentDidMount(){
-debugger
-    }
- renderLists() {
-      /*
-        this.props.books.map( (book,index)=> {
-    console.log(book.title);            
-            return (
-                <li key={index} className="list-group-item">
-             {book.title}
-             </li>
-            );
-        });
-        */
-    }
     render() {
-   debugger;
-   console.log(this.props.books)
+        debugger;
+        console.log(this.props.books)
         return (
-
-          <div>
-          <h3>Hello React</h3>
-            <ul>               
+            <div>
                 {
-                    this.props.books.map( (book,index) => {   
-                        console.log(book)  ;
-                        console.log(book.title);                                      
-                    <li key={index}>
-                      {index}{book.title}
-                    </li>
+                    this.props.books.map((book, index) => {
+                        console.log(book.title);
+                        return (
+                            <li key={index} >
+                                {book.title}
+                            </li>
+                        );
                     })
-                }       
-            </ul>
+                }
             </div>
         )
     }
-    
-   
 }
 
 function mapStateToProps(state) {//application state connection there between redux and react
-    debugger;
-    return  { 
+    return {
         books: state.books
     }
 
