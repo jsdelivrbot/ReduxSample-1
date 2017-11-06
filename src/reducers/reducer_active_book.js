2d6b0ce6316c83bfcea5a5fcf4ff5f362477d762
Nikita Deshmukh <ndeshmukh@galaxyps89.com>
2017-11-06 11:40:43 -0500

    BOOK_SELECTED

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,9 @@
 //here reducer will call whenever action is dispatched on call
-export default function(state,action)
+export default function(state=null,action)
 {
-
+    switch(action.type){
+        case 'BOOK_SELECTED':
+            return action.payload;
+   }
+return state
 }
